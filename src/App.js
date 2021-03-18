6b499aea1a38841af6ed533b9b0591d7f5837e7d
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2021-03-18 18:39:42 -0400

    add background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import { Grid } from "./Grid";
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: rgb(16, 22, 47);
+  padding: 40px;
+`;
+
+const GameContainer = styled.div`
+  height: 700px;
+  width: 700px;
+  background-color: rgb(20, 28, 58);
+  border: 1px solid rgb(45, 93, 204);
+  border-radius: 5px;
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      <GameContainer>
+        <Grid />
+      </GameContainer>
+    </PageContainer>
   );
 }
 
